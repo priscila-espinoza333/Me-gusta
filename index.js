ca0b1e3d9023033444ea00218aca23d0467ab39e
Pris <pr.espinoza@duocuc.cl>
2023-09-19 23:33:24 -0300

    parte 2

--- index.js
+++ index.js
@@ -11,3 +11,33 @@ function updateDisplay(val) {
         counterLabel.textContent = val;
     }
 }
+
+// Parte 2
+
+var counterVal1 = 9;
+var counterVal2 = 12;
+var counterVal3 = 9;
+
+function incrementClick1() {
+    counterVal1++;
+    updateDisplay("counter-label1", counterVal1);
+}
+
+function incrementClick2() {
+    counterVal2++;
+    updateDisplay("counter-label2", counterVal2);
+    
+}
+
+function incrementClick3() {
+    counterVal3++;
+    updateDisplay("counter-label3", counterVal3);
+}
+
+function updateDisplay(elementId, val){
+    var counterLabel = document.getElementById(elementId);
+    if(counterLabel){
+        counterLabel.textContent = val;
+    }
+}
+
